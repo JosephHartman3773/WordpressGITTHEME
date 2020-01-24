994e8a3617326bb4b6b5a161321cab0aa73a332a
Joes MacNCheese <admin@Admins-MacBook-Pro.local>
2020-01-24 10:29:04 -0500

    9

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header();
 	<div id="primary" class="content-area">
 	
 	<img class="what-ever" src="<?php bloginfo('template_directory');
-	 ?>/images/10.jpg" border="0" alt="">
+	 ?>images/10.jpg" border="0" alt="">
 
 	</div><!-- #primary -->
 
