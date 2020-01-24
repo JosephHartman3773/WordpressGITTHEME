864f70c1556c9de69ef0ec8781a5899489b9b619
Joes MacNCheese <admin@Admins-MacBook-Pro.local>
2020-01-24 10:08:49 -0500

    yesss

--- 404.php
+++ 404.php
@@ -14,6 +14,9 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+			<img class="what-ever" src="<?php bloginfo('template_directory');
+			 ?>/Assests/images/1.jpg" border="0" alt="">
+			 
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'thedude' ); ?></h1>
