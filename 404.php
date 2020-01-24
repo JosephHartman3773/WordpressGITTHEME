2eab343569716ad48365a5bc401a41864e0a65d8
Joes MacNCheese <admin@Admins-MacBook-Pro.local>
2020-01-24 10:11:22 -0500

    adsf

--- 404.php
+++ 404.php
@@ -9,8 +9,7 @@
 
 get_header();
 ?>
-	<img class="what-ever" src="<?php bloginfo('template_directory');
-	 ?>/Assests/images/1.jpg" border="0" alt="">
+	
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
