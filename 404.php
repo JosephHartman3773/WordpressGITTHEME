cfcc6e644deff2690efa1c831bd89232a0c68ce7
Joes MacNCheese <admin@Admins-MacBook-Pro.local>
2020-01-24 11:20:34 -0500

    17

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header();
 	<img class="what-ever" src="<?php bloginfo('template_directory');
 	 ?>/assets/images/10.jpg" border="0" alt="">
 	 
-	 <img src="jhartman18/wordpress/wp-content/themes/WordpressGITTHEME/assets/images/10.jpg">
+	 <img src="https://i.ytimg.com/vi/qD2LW6naK-g/maxresdefault.jpg">
 
 
 	</div><!-- #primary -->
