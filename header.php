405eb76bdd166a510b12ebcc3951e36d54f8889f
Joes MacNCheese <admin@Admins-MacBook-Pro.local>
2020-01-30 11:41:09 -0500

    37

--- header.php
+++ header.php
@@ -53,6 +53,17 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
+
+		<img alt="" src="<?php header_image(); ?>" width="<?php echo thedude( get_custom_header()->width ); 
+		?>" height="<?php echo thedude( get_custom_header()->height ); ?>">
+		
+		<?php if ( get_header_image() ) : ?>
+    <div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php header_image(); ?>" width="<?php echo thedude( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+        </a>
+    </div>
+<?php endif; ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
